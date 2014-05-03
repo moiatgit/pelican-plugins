3895f099d4c9c7b821aebfdd25fa78b37dce64d1
Moisès Gómez Girón <moisesgomezgiron@gmail.com>
2014-05-03 19:02:56 +0200

    added insertion of top information

--- navigationprevnext/navigationprevnext.py
+++ navigationprevnext/navigationprevnext.py
@@ -13,15 +13,21 @@ import os
 
 def compose_entry_with_category(entry, category):
     """ returns entry prepending category if not already there """
-    catpath = os.path.join(category, "")
-    return entry if entry.startswith(catpath) else os.path.join(category, entry)
+    if os.path.dirname(entry) == "":
+        composed_entry = os.path.join(category, entry)
+    else:
+        composed_entry = entry
+    return composed_entry
 
-def translate_metaslug(generator):
-    """ translate the references in source to final """
+def list_articles(generator):
+    """ returns a dictionary with articles keyed by its relative source path """
     articles = {}
     for article in generator.articles:
         articles[article.get_relative_source_path()] = article
+    return articles
 
+def insert_prev_next(articles):
+    """ insert prev/next information into articles """
     for article in articles.values():
         if hasattr(article, 'next_entry'):
             category_name = article.category.name
@@ -34,6 +40,30 @@ def translate_metaslug(generator):
             article.next_title = next_article.title
             next_article.prev_url = article.url
             next_article.prev_title = article.title
+
+def insert_top(articles):
+    """ insert top information into articles """
+    for article in articles.values():
+        if hasattr(article, 'next_url') and not hasattr(article, 'prev_url'):
+            top_url = article.url
+            category_name = article.category.name
+            next_entry = compose_entry_with_category(article.next_entry, category_name)
+            current_article = articles.get(article.next_entry, None)
+            while current_article != None:
+                current_article.top_url = top_url
+                current_article.top_title = "inici"
+                if hasattr(current_article, 'next_entry'):
+                    category_name = current_article.category.name
+                    next_entry = compose_entry_with_category(current_article.next_entry, category_name)
+                    current_article = articles.get(next_entry, None)
+                else:
+                    current_article = None
+
+def translate_metaslug(generator):
+    """ translate the references in source to final """
+    articles = list_articles(generator)
+    insert_prev_next(articles)
+    insert_top(articles)
 
 def register():
     signals.article_generator_finalized.connect(translate_metaslug)
